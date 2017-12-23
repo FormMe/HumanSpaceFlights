a4553ae51481ea48657a76956d8b8eb273750a21
FormMe <formme1995@yandex.ru>
2017-12-23 19:45:20 +0300

    missions data aggregation

--- js/script.js
+++ js/script.js
@@ -3,5 +3,36 @@ let flightsChart = new FlightsChart();
 
 
 d3.csv("data/missions.csv", function (error, missionsData) {
+
+    d3.nest()
+        .key(function(d) { return d['Launch Mission']; })
+        .rollup(function(v) {
+            var info = v[0];
+            obj = {
+                "Brief Mission Summary": info["Brief Mission Summary"],
+                "Country": info["Country"],
+                "Fatality": info["Fatality"],
+                "Moon": info["Moon"],
+                "Sub Orbital": info["Sub Orbital"],
+                "Launch Data": info["Launch Data"],
+                "Launch Mission": info["Launch Mission"],
+                "Launch Year": info["Launch Year"],
+                "Habitation": info["Habitation"],
+                "Crew": v.map(function (part) {
+                    return {
+                        "Members": part["Crew"].split(',').map(name => name.trim()),
+                        "Prolongation": part["Prolongation"],
+                        "Return Data": part["Return Data"],
+                        "Return Mission": part["Return Mission"]
+                    }
+                })
+
+            }
+            console.log(v, obj); 
+            return obj;
+        }) 
+        .entries(missionsData)
+
+
     flightsChart.update(missionsData);
 });
